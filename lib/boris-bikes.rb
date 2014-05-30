fa0da62c2ca24ba31a6ae9de5e1689f43d6072ef
Nadav <nadav.matalon@gmail.com>
2014-05-30 10:51:20 +0100

    added boris-bikes file to require all other files in lib

---
+++ lib/boris-bikes.rb
@@ -0,0 +1,39 @@
+require_relative 'bike.rb'
+require_relative 'station.rb'
+require_relative 'van.rb'
+require_relative 'garage.rb'
+require_relative 'person.rb'
+require_relative 'bike_container.rb'
+
+
+@bike = Bike.new
+@bike_1 = Bike.new
+@bike_2 = Bike.new
+@bike_3 = Bike.new
+@bike_4 = Bike.new
+@bike_5 = Bike.new
+@bike_6 = Bike.new
+@bike_7 = Bike.new
+@bike_8 = Bike.new
+@bike_9 = Bike.new
+@bike_11 = Bike.new
+
+@station = Station.new
+@station_1 = Station.new
+@station_2 = Station.new
+
+@van = Van.new
+@van_1 = Van.new
+@van_2 = Van.new
+
+@garage = Garage.new
+@garage_1 = Garage.new
+@garage_2 = Garage.new
+
+@person = Person.new
+@person_1 = Person.new
+@person_2 = Person.new
+
+
+
+
